cfc21759f53534a8fdf4c35969c79e6e5aae2b14
Akseli <akseli.ojanen@tuni,fi>
2023-08-17 21:42:59 +0300

    new luvut 2

--- osa1/luvut_2.c
+++ osa1/luvut_2.c
@@ -3,8 +3,9 @@
 #include <string.h>
 
 int is_div(int num, int *div, int size){
-	for (int i = 0; i < size; ++size){
-		if ( num / div[i] == 0 ){
+	int i;
+	for (i = 0; i < size; size++){
+		if ( num % div[i] == 0 ){
 			return 1;
 		}
 	}
@@ -15,36 +16,38 @@ int main(int argc, char *argv[]){
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 	int divCount = argc - 3;
-	int divisors[divCount];
+	int *divisors;
 	int printed = 0;
+	int i;
 
 	if (argc == 3){
-		for (int i = a; i < b; ++i){
+		for (i = a; i < b; i++){
 			printf("%d ", i);
 		}
 		printf("%d\n", b);
 		return 0;
 	}
 
-	for (int i = 0; i < divCount; ++i){
+	divisors = (int *)malloc(divCount * sizeof(int));
+	for (i = 0; i < divCount; i++){
 		divisors[i] = atoi(argv[i + 3]);
 	}
 
-	 for (int i = a; i < b; ++i) {
-        if (!is_divisible(i, divisors, divCount)) {
+	for (i = a; i < b; i++) {
+        if (!is_div(i, divisors, divCount)) {
 			if (printed) {printf(" ");}
             printf("%d", i);
 			++printed;
         }
-		if (!is_div(b, divisors, divCount)){
+    }
+	if (!is_div(b, divisors, divCount)){
 			if (printed) {printf(" ");}
 			printf("%d\n", b);
+		} else {
+			printf("\n");
 		}
-	return 0;
-    }
 
+	free(divisors);
 	
-
-
-	
+	return 0;	
 }
